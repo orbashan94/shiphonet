b0d72facb85f7e19d120b2274180e15e5b62fe2d
orbashan94 <orbashan94@gmail.com>
2023-06-01 14:37:41 +0300

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import logo from "../assets/images/logo.png";
 
-const Navbar = () => {
+const Navbar = ({ setCartIsShown }) => {
   return (
     <header>
       <nav className="navbar">
         <img className="logo" src={logo}></img>
-        <div className="cart-badge">
+        <button
+          className="cart-badge"
+          onClick={() => {
+            setCartIsShown(true);
+          }}
+        >
           <span className="badge">1</span>
           <ShoppingCartIcon style={{ fontSize: "30px" }} />
-        </div>
+        </button>
       </nav>
     </header>
   );
