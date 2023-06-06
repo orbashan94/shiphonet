9ee97b6bb47d696010cf5b974d9c86a5ee316af4
orbashan94 <orbashan94@gmail.com>
2023-06-06 09:57:58 +0300

    fix ImageDialog

--- src/components/ImageDialog.jsx
+++ src/components/ImageDialog.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import defaultImage from "../assets/images/logo.png";
 
 const ImageDialog = ({ image, setDisplayDialog }) => {
+
+  let img = "./meals/";
+
+  if (!image) {
+    img = defaultImage;
+  }
+
   return (
     <div className="dialog-container" onClick={() => setDisplayDialog(false)}>
       <div className="dialog">
-        <img src={image ? `./${image}` : defaultImage} alt="the image" />
-        <h1>sdvdsvds</h1>
+        <img src={image ? img + image : img} alt="the image" />
       </div>
     </div>
   );
